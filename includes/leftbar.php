e2d0c49dc90b0b6750a5f976cb82ff50b64ee14c
kelzbiggie01 <kevmayeye97@gmail.com>
2022-06-17 11:11:13 +0300

    working with add score page

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -49,8 +49,8 @@
                                       <li class="has-children">
                                         <a href="#"><i class="fa fa-users"></i> <span>Score</span> <i class="fa fa-angle-right arrow"></i></a>
                                         <ul class="child-nav">
-                                            <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Add Score</span></a></li>
-                                            <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>View Score</span></a></li>
+                                            <li><a href="add-score.php"><i class="fa fa-bars"></i> <span>Add Score</span></a></li>
+                                            <li><a href="view-score.php"><i class="fa fa fa-server"></i> <span>View Score</span></a></li>
                                            
                                         </ul>
                                     </li>
