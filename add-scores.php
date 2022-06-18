616fb8e05c0a2180c88cee115444d06481381121
kelzbiggie01 <kevmayeye97@gmail.com>
2022-06-18 13:31:42 +0300

    changed score page title

--- add-scores.php
+++ add-scores.php
@@ -9,7 +9,7 @@ include('includes/config.php');
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>SMS Admin| Subject Admission< </title>
+        <title>SMS Admin| Add Score </title>
         <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
         <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
         <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
