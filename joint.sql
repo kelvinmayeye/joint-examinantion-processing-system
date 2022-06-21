940d9c81fc90b82ef36b00fe0dd7f55cc0778bd9
kelzbiggie01 <kevmayeye97@gmail.com>
2022-06-21 21:18:52 +0300

    added database file

---
+++ joint.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 21, 2022 at 08:16 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `joint`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade`
+--
+
+CREATE TABLE `grade` (
+  `id` int(15) NOT NULL,
+  `gradename` varchar(100) NOT NULL,
+  `start_value` int(10) NOT NULL,
+  `end_value` int(10) NOT NULL,
+  `grade_point` varchar(100) NOT NULL,
+  `comments` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `school`
+--
+
+CREATE TABLE `school` (
+  `regno` varchar(100) NOT NULL,
+  `schoolname` varchar(100) NOT NULL,
+  `schooltype` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `school`
+--
+
+INSERT INTO `school` (`regno`, `schoolname`, `schooltype`) VALUES
+('023MU', 'another scho', 'private'),
+('122', 'aasass', 'private'),
+('123', 'mzumbe', 'private'),
+('123O', 'school', 'government'),
+('hff', '2933', 'private'),
+('hwehe', 'jeue', 'private'),
+('jkewfeuew', '233', 'private'),
+('MU3902', 'The School Name', 'cooperative'),
+('SCHOOL0123', 'School of Hardknocks', 'private');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scores`
+--
+
+CREATE TABLE `scores` (
+  `sid` int(5) NOT NULL,
+  `subcode` int(100) NOT NULL,
+  `score` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `sid` int(5) NOT NULL,
+  `sch_id` varchar(100) NOT NULL,
+  `f_name` varchar(100) NOT NULL,
+  `m_name` varchar(100) DEFAULT NULL,
+  `l_name` varchar(100) NOT NULL,
+  `sex` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`sid`, `sch_id`, `f_name`, `m_name`, `l_name`, `sex`) VALUES
+(268, '123O', 'jdjjf', 'm_name', 'l_name', 'M'),
+(439, 'SCHOOL0123', 'Annalisa', 'G', 'Kashaga', 'F'),
+(504, 'SCHOOL0123', 'Kelvin', 'm_name', 'l_name', 'M'),
+(621, '123O', 'jdjjf', 'm_name', 'l_name', 'M');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subcode` int(100) NOT NULL,
+  `subname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`subcode`, `subname`) VALUES
+(111, 'Kiswahili'),
+(123, 'Science');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `sno` int(100) NOT NULL,
+  `sch_id` varchar(100) NOT NULL,
+  `f_name` varchar(100) NOT NULL,
+  `m_name` varchar(100) DEFAULT NULL,
+  `l_name` varchar(100) NOT NULL,
+  `sex` char(1) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`sno`, `sch_id`, `f_name`, `m_name`, `l_name`, `sex`, `username`, `password`, `role`) VALUES
+(1, '123', 'userone', 'm', 'lastname', 'f', 'userone', '202cb962ac59075b964b07152d234b70', 'user'),
+(2, '123', 'sir', NULL, 'mister', 'm', 'sir', '202cb962ac59075b964b07152d234b70', 'admin'),
+(3, '122', 'qwiuw', 'i', 'wihuw', 'm', 'mal', '202cb962ac59075b964b07152d234b70', 'user'),
+(5, 'SCHOOL0123', 'firstname_OG', 'second_OG', 'lastname_OG', 'F', 'Tr Konde', '202cb962ac59075b964b07152d234b70', 'teacher');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `school`
+--
+ALTER TABLE `school`
+  ADD PRIMARY KEY (`regno`);
+
+--
+-- Indexes for table `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`sid`),
+  ADD KEY `subcode` (`subcode`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`sid`),
+  ADD KEY `sch_id` (`sch_id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subcode`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`sno`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `sch_id` (`sch_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `sid` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `sno` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `scores`
+--
+ALTER TABLE `scores`
+  ADD CONSTRAINT `scores_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `scores_ibfk_2` FOREIGN KEY (`subcode`) REFERENCES `subject` (`subcode`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`sch_id`) REFERENCES `school` (`regno`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`sch_id`) REFERENCES `school` (`regno`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
